1253a96e75e8201fa498a130146831df31ab3bd2
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2026-09-15 09:33:11 +0200

    add background

--- main.py
+++ main.py
@@ -14,37 +14,50 @@ def main():
     pg.font.init()
     # Donne un nom à la fenêtre
     pg.display.set_caption("PONG")
-    # Taille de l'écran
-    screenSize = (1024,768)
-    # Crée la surface qui va servir de surface de jeu
-    screen = pg.display.set_mode(screenSize)
+
+    # Ratio du moniteur (ex: 16/9)
+    monitor = pg.display.Info()
+    aspect_ratio = monitor.current_w / monitor.current_h
+
+    # Résolution fixe du jeu (ratio moniteur)
+    GAME_W = 1024
+    GAME_H = int(GAME_W / aspect_ratio)
+    game_surface = pg.Surface((GAME_W, GAME_H))
+
+    # Fenêtre redimensionnable, taille initiale = résolution du jeu
+    screen = pg.display.set_mode((GAME_W, GAME_H), pg.RESIZABLE)
+
     # Crée un objet horloge pour gerer le temps entre deux images
     clock = pg.time.Clock()
-    # Nombre de millisecondes entre deux images
-    dt = 0
-    # Création d'une instance du jeu, donne l'écran où il faut dessiner
-    pong = Eclilpsoide(screen)
+    # Création d'une instance du jeu, donne la surface de rendu fixe
+    eclipsoide = Eclilpsoide(game_surface)
 
     # Boucle de jeu
-    while pong.isRunning():
-        # Limite la vitesse à 6O images max par secondes
+    while eclipsoide.isRunning():
+        # Limite la vitesse à 60 images max par secondes
         # Calcule le temps réel entre deux images en millisecondes
         dt = clock.tick(60)
 
         # Met à jour le jeu sachant que dt millisecondes se sont écoulées
-        pong.update(dt)
+        eclipsoide.update(dt)
 
-        # Demande au jeu d'afficher sur l'écran (screen) son nouvel état
-        pong.draw()
+        # Demande au jeu d'afficher sur la surface de rendu son nouvel état
+        eclipsoide.draw()
+
+        # Scale la surface de rendu pour remplir la fenêtre en gardant le ratio
+        win_w, win_h = screen.get_size()
+        scale_w = win_w
+        scale_h = int(win_w / aspect_ratio)
+        if scale_h > win_h:
+            scale_h = win_h
+            scale_w = int(win_h * aspect_ratio)
+
+        scaled = pg.transform.scale(game_surface, (scale_w, scale_h))
+        screen.fill((0, 0, 0))
+        screen.blit(scaled, ((win_w - scale_w) // 2, (win_h - scale_h) // 2))
 
         # Bascule le nouvel état de l'écran
         pg.display.flip()
-
-    # Attend 4s avant de fermer la fenêtre
-    t = 0
-    while t < 4000:
-        dt = clock.tick(60)
-        t += dt
 
     # Fin utilisation de pygame
     pg.quit()
