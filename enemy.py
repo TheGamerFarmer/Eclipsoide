a8e3ec21be4042eab2c81e68a453208738ef8e3b
Eptaziii <dignoire.erwan@gmail.com>
2026-09-15 14:25:27 +0200

    dégâts aux ennemies

--- enemy.py
+++ enemy.py
@@ -65,3 +65,8 @@ class Enemy(pg.sprite.Sprite):
 
         self.rect.x = int(newPos.x)
         self.rect.y = int(newPos.y)
+
+    def hited(self, damage: int):
+        self.life -= damage
+        if self.life <= 0:
+            self.kill()
