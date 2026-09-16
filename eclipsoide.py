779fe8f3e4ed444fd7f27abb391ebd929213a85a
Florian  Esdras. <86320761+Esdro@users.noreply.github.com>
2026-09-16 10:58:30 +0200

    amélioration du boss avec ux du niveau

--- eclipsoide.py
+++ eclipsoide.py
@@ -40,6 +40,8 @@ class Eclipsoide:
     BOSS_MAX_SIZE = 620
     # Chaque boss vaincu rend le suivant 1,5 fois plus résistant
     BOSS_LIFE_GROWTH = 1.5
+    # Points gagnés en tuant un boss, multipliés par son palier
+    BOSS_REWARD = 100
 
     # Délai (explosion du joueur) avant d'afficher l'écran de game over
     DEATH_COOLDOWN = 1300  # ms
@@ -143,6 +145,11 @@ class Eclipsoide:
 
     def _boss_vaincu(self):
         """ Le boss explose, le palier suivant démarre : les vagues reprennent """
+        centre = pg.Vector2(self.boss.rect.center)
+        # Récompense : 100 points par palier du boss vaincu
+        gain = self.BOSS_REWARD * self.boss_level
+        self.player.add_coins(gain)
+        CoinPopup(centre, gain, self.popups_group)
         Explosion(pg.Vector2(self.boss.rect.center), self.explosions_group)
         self.boss.bombs.empty()
         self.boss.kill()
